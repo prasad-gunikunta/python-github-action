710871e973a825bb151545b73fd25556184fda2a
prasadg <prasad.g@hpe.com>
2025-03-13 10:53:13 -0400

    Enhanced test cases

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -5,8 +5,10 @@ def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
     assert add(5,3)==8
+    assert add(5,15)==20
 
 def test_sub():
     assert sub(3,2)==1
     assert sub(1,1)==0
     assert sub(3,5)==-2
+    assert sub(13,5)==8
